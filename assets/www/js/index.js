3c013a7a921b33a1c14f05eed77119d09b704087
Isac Sandin Ribeiro <isacsandim@gmail.com>
2014-10-09 09:53:51 -0300

    adicionando menu lateral e removendo abas, adicionando formulario de login

--- assets/www/js/index.js
+++ assets/www/js/index.js
@@ -46,3 +46,24 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
+
+//$( document ).on( "pagecreate", "#demo-page", function() {
+//    $( document ).on( "swipeleft swiperight", "#demo-page", function( e ) {
+//        // We check if there is no open panel on the page because otherwise
+//        // a swipe to close the left panel would also open the right panel (and v.v.).
+//        // We do this by checking the data that the framework stores on the page element (panel: open).
+//        //if ( $( ".ui-page-active" ).jqmData( "panel" ) == "open" ) {
+//            if ( e.type === "swipeleft" ) {
+//            	console.log("swipeleft");
+//                $( "#left-panel" ).panel( "close" );
+//            } 
+//        //}
+//        //else{
+//            else {
+//            	console.log("swiperight");
+//                $( "#left-panel" ).panel( "open" );
+//            }
+//        //}
+//    });
+//});
+
